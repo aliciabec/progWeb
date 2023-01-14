5c8cd419eb83db9e36ced9eae9258f2248e9bc76
aliciabec <aliciabec@icloud.com>
2023-01-14 17:41:05 +0100

    ajout base presque fini

--- progWeb/progWeb/settings.py
+++ progWeb/progWeb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projet.apps.ProjetConfig',
+    #'django_extensions',
 ]
 
 MIDDLEWARE = [
--- progWeb/projet/models.py
+++ progWeb/projet/models.py
@@ -2,62 +2,34 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-class Test(models.Model):
-    text = models.CharField(max_length=200)
-    def __str__(self):
-        return self.text
-
-
 class Utilisateur(models.Model):
-    email = models.CharField(max_length=100,primary_key=True)
+    email = models.EmailField(primary_key=True)
     nom = models.CharField(max_length=30)
     prenom = models.CharField(max_length=30)
     mot_de_passe= models.CharField(max_length=30)
-    tel = models.IntegerField(max_length=30)
+    tel = models.IntegerField()#max_length=30)
+    role = models.CharField(max_length=100,choices=(("annot","annotateur"),("val", "validateur"),("user","utilisateur")),default="user")
     # Rajouter date et heure, il doit exister une fonction pour ça
-
-class Annotateur(models.Model):
-    email = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
-
-class Validateur(models.Model):
-    email = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
- 
 
 class Genome(models.Model):
     Id_genome = models.CharField(max_length=100,primary_key=True)
-    contenu_genome = models.CharField(max_length=10000000)
-    email = models.ForeignKey(Utilisateur, on_delete=models.CASCADE) # clé etrangere de utilisateur
+    taille_sequence = models.CharField(max_length=100,blank=True,null=True)
+    sequence_genome = models.TextField(null=True,blank=True)
 
-class Gene(models.Model):
-    Id_genome = models.ForeignKey(Genome, on_delete=models.CASCADE) # clé etrangere de utilisateur de genome
 
-class Annotaté(models.Model):
-    Id_genome = models.ForeignKey(Genome, on_delete=models.CASCADE) # clé etrangere de genome
-    espece = models.CharField(max_length=100)
+class Annotation(models.Model):
+    Id_genome = models.ForeignKey(Genome,on_delete=models.CASCADE)#,primary_key=True)
+    emailAnnotateur = models.CharField(max_length=100,null=True)#ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    emailValidateur = models.CharField(max_length=100,null=True)#ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    estValide = models.BooleanField(default=False)
+    estAnnote = models.BooleanField(default=False)
+    commentaire = models.CharField(max_length=1000000,null=True)
 
-class Attribué(models.Model):
-    Id_genome = models.ForeignKey(Genome, on_delete=models.CASCADE) # clé etrangere de genome
-    email_annot = models.CharField(max_length=100)
-    email_valid = models.CharField(max_length=100)
-    validé = #true ou false
-    commentaire = models.CharField(max_length=1000000)
-
-class Chaine_prot(models.Model):
+class gene_prot(models.Model):
     Id_genome = models.ForeignKey(Genome, on_delete=models.CASCADE) # clé etrangere de genome
     nom_gene = models.CharField(max_length=100,primary_key=True)
-    description = models.CharField(max_length=1000000)
-
-class Gene(models.Model):
-    Id_genome = models.ForeignKey(Genome, on_delete=models.CASCADE) # clé etrangere de genome
-    nom_gene = models.CharField(max_length=100,primary_key=True)
-    start_position = models.IntegerField(max_length=30)
-    end_position = models.IntegerField(max_length=30)
-    sequence_nucleotidique = models.CharField(max_length=10000000)
-
-
-class proteine(models.Model):
-    Id_genome = models.ForeignKey(Genome, on_delete=models.CASCADE) # clé etrangere de genome
-    nom_gene = models.CharField(max_length=100,primary_key=True)
-    start_position = models.IntegerField(max_length=30)
-    end_position = models.IntegerField(max_length=30)
-    sequence_peptidique = models.CharField(max_length=10000000)
+    start_position = models.IntegerField()#max_length=30)
+    end_position = models.IntegerField()#max_length=30)
+    description = models.CharField(max_length=1000000,null=True)
+    sequence_nucleotidique = models.TextField(null=True)
+    sequence_peptidique = models.TextField(null=True)

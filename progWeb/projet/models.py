48450b32b3cc5c324e8a7c54184a9fe648773736
aliciabec <aliciabec@icloud.com>
2023-01-18 10:30:17 +0100

    new version BD

--- progWeb/projet/models.py
+++ progWeb/projet/models.py
@@ -10,17 +10,26 @@ class Utilisateur(models.Model):
     tel = models.IntegerField()#max_length=30)
     role = models.CharField(max_length=100,choices=(("annot","annotateur"),("val", "validateur"),("user","utilisateur")),default="user")
     # Rajouter date et heure, il doit exister une fonction pour ça
+    
 
 class Genome(models.Model):
     Id_genome = models.CharField(max_length=100,primary_key=True)
     taille_sequence = models.CharField(max_length=100,blank=True,null=True)
     sequence_genome = models.TextField(null=True,blank=True)
 
+class Annotateur(models.Model):
+    Id_genome = models.ForeignKey(Utilisateur,on_delete=models.CASCADE)#,primary_key=True)
+    emailAnnotateur = models.CharField(max_length=100,null=True)#ForeignKey(Utilisateur, on_delete=models.CASCADE)
+
+class Validateur(models.Model):
+    Id_genome = models.ForeignKey(Utilisateur,on_delete=models.CASCADE)#,primary_key=True)
+    emailValidateur = models.CharField(max_length=100,null=True)#ForeignKey(Utilisateur, on_delete=models.CASCADE)
+
 
 class Annotation(models.Model):
     Id_genome = models.ForeignKey(Genome,on_delete=models.CASCADE)#,primary_key=True)
-    emailAnnotateur = models.CharField(max_length=100,null=True)#ForeignKey(Utilisateur, on_delete=models.CASCADE)
-    emailValidateur = models.CharField(max_length=100,null=True)#ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    emailAnnotateur = models.ForeignKey(Annotateur, on_delete=models.CASCADE,null=True)
+    emailValidateur = models.ForeignKey(Validateur, on_delete=models.CASCADE,null=True)
     estValide = models.BooleanField(default=False)
     estAnnote = models.BooleanField(default=False)
     commentaire = models.CharField(max_length=1000000,null=True)

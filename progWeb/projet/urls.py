11ea05d105d66231773c6dea7ea32f4915b5fc4c
Hao <kimhao3701@gmail.com>
2023-01-25 10:55:54 +0100

    ajout

--- progWeb/projet/urls.py
+++ progWeb/projet/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('connexion/', views.Connexion.as_view(), name='connexion'),
     path('inscription/', views.Inscription.as_view(), name='inscription'),
     path('annotation/', views.Annotation.as_view(), name='annotation'),
+    path('annot/', views.Annot.as_view(), name='annot'),
+    path('r1/', views.R1.as_view(), name='r1'),
+    path('r2/', views.R2.as_view(), name='r2'),
 ]

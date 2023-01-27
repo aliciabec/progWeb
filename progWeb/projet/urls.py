330a39bd7d0b632c469f3138bd56f37e3c78fa8b
aliciabec <aliciabec@icloud.com>
2023-01-27 16:52:13 +0100

    modification de accueil dans view

--- progWeb/projet/urls.py
+++ progWeb/projet/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = 'projet'
 urlpatterns = [
-    path('', views.Accueil.as_view(), name='accueil'),
+    path('', views.accueil, name='accueil'),
     path('connexion/', views.Connexion.as_view(), name='connexion'),
-    path('inscription/', views.Inscription.as_view(), name='inscription' ),
-    path('inscription/createuser', views.Inscription.create_user, name='inscription2' ),
+    path('inscription/', views.Inscription.create_user, name='inscription' ),
+    #path('inscription/createuser', views.Inscription.create_user, name='inscription2' ),
     path('annotation/', views.Annotation.as_view(), name='annotation'),
     path('thanks/', views.Thanks.as_view(), name='thanks'),
     path('annot/', views.Annot.as_view(), name='annot'),

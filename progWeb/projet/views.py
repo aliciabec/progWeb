ed3fd4a67b3e109fe8749d754c0ec9937f208ecb
FNajlae <Tata@tata.com>
2023-01-27 16:30:13 +0100

    miseàjbranche

--- progWeb/projet/views.py
+++ progWeb/projet/views.py
@@ -20,4 +20,110 @@ class Accueil(generic.ListView):
 #        published in the future).
 #        """
         print(self.request.user)
+<<<<<<< Updated upstream
         return Test.objects
+=======
+        return 0
+
+
+
+class Connexion(generic.ListView):
+    template_name = 'projet/connexion.html'
+    def get_queryset(self):
+#        """
+##        Return the last five published questions (not including those set to be
+#        published in the future).
+#        """
+        print(self.request.user)
+        return 0
+
+from django.template import RequestContext
+
+
+class Inscription(generic.ListView):
+    template_name = 'projet/inscription.html'
+    def get_queryset(self):
+#        
+##       Return the last five published questions (not including those set to be
+#        published in the future).
+#      
+        return 0
+    
+    def create_user(self):   
+        ## test pour le role 
+        roles = ""
+        print(self.GET)
+        print(self.POST)
+        print(self.POST.keys())
+        if self.POST['roles'] == "lecteur":
+            roles = 'user'
+        elif self.POST['roles'] == "annotateur":
+            roles = 'annot'
+        elif self.POST['roles'] == "validateur":
+            roles = 'val'
+        Utilisateur(email = self.POST['email']
+                    , nom = self.POST['nom']
+                    , prenom= self.POST['prenom']
+                    , mot_de_passe = self.POST['pass_word_id']
+                    , tel = self.POST['tel']
+                    , roles = roles).save()
+
+        return HttpResponseRedirect('/projet/connexion/', RequestContext(self) )
+
+
+
+class Annotation(generic.ListView):
+    template_name = 'projet/annotation.html'
+    def get_queryset(self):
+#        """
+##        Return the last five published questions (not including those set to be
+#        published in the future).
+#        """
+        print(self.request.user)
+        return 0
+
+
+
+class Thanks(generic.ListView):
+    template_name = 'projet/thanks.html'
+    def get_queryset(self):
+#        """
+##        Return the last five published questions (not including those set to be
+#        published in the future).
+#        """
+        print(self.request.user)
+        return 0
+
+
+class Annot(generic.ListView):
+    template_name = 'projet/annot.html'
+    def get_queryset(self):
+#        """
+##        Return the last five published questions (not including those set to be
+#        published in the future).
+#        """
+        print(self.request.user)
+        return 0
+
+
+class R1(generic.ListView):
+    template_name = 'projet/r1.html'
+    def get_queryset(self):
+#        """
+##        Return the last five published questions (not including those set to be
+#        published in the future).
+#        """
+        print(self.request.user)
+        return 0
+
+
+class R2(generic.ListView):
+    template_name = 'projet/r2.html'
+    def get_queryset(self):
+#        """
+##        Return the last five published questions (not including those set to be
+#        published in the future).
+#        """
+        print(self.request.user)
+        return 0
+>>>>>>> Stashed changes

08606882b0e82daded6da6c7056a8340d2c79561
Maïsen Hassani <maisen992@gmail.com>
2023-02-03 14:37:00 +0100

    ajout

--- progWeb/projet/urls.py
+++ progWeb/projet/urls.py
@@ -3,10 +3,6 @@ from . import views
 
 app_name = 'projet'
 urlpatterns = [
-    path('', views.Accueil.as_view(), name='accueil'),
-    path('connexion/', views.Connexion.as_view(), name='connexion'),
-    path('inscription/', views.Inscription.as_view(), name='inscription' ),
-    path('inscription/createuser', views.Inscription.create_user, name='inscription2'),
     path('', views.accueil, name='accueil'),
     path('connexion/', views.Connexion.as_view(), name='connexion'),
     path('inscription/', views.Inscription.create_user, name='inscription' ),
@@ -15,7 +11,6 @@ urlpatterns = [
     path('thanks/', views.Thanks.as_view(), name='thanks'),
     path('annot/', views.Annot.as_view(), name='annot'),
     path('r1/', views.R1.as_view(), name='r1'),
-    path('r2/', views.R2.as_view(), name='r2'),
     path('r2/', views.R2.get_queryset, name='r2'),
 ]
  
